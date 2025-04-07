5e9a3151fc1c49ae2b21c51c678d0a498f6059ca
amart <amart@192.168.1.147>
2025-04-06 20:18:27 -0500

    codigo base ciclista

--- src/Clases/Ciclista.java
+++ src/Clases/Ciclista.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
-/**
- *
- * @author amart
- */
 public class Ciclista {
-    
+    private int identificador;
+    private String nombre;
+    private int tiempoAcumulado;
+
+    public Ciclista(int identificador, String nombre) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.tiempoAcumulado = 0;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTiempoAcumulado() {
+        return tiempoAcumulado;
+    }
+
+    public void setTiempoAcumulado(int tiempo) {
+        this.tiempoAcumulado = tiempo;
+    }
+
+    public void imprimirDatos() {
+        System.out.println("Ciclista: " + nombre + " (ID: " + identificador + ")");
+        System.out.println("Tiempo Acumulado: " + tiempoAcumulado);
+    }
 }
