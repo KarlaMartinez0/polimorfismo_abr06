00a46ad1521c5849c0466d122a498b3ab6ee66a1
amart <amart@192.168.1.147>
2025-04-06 20:19:26 -0500

    codigo base escalador

--- src/Clases/Escalador.java
+++ src/Clases/Escalador.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
-/**
- *
- * @author amart
- */
-public class Escalador {
-    
+public class Escalador extends Ciclista {
+    private float aceleracionPromedio;
+    private float gradoRampaSoportada;
+
+    public Escalador(int identificador, String nombre, float aceleracionPromedio, float gradoRampaSoportada) {
+        super(identificador, nombre);
+        this.aceleracionPromedio = aceleracionPromedio;
+        this.gradoRampaSoportada = gradoRampaSoportada;
+    }
+
+    public float getAceleracionPromedio() {
+        return aceleracionPromedio;
+    }
+
+    public void setAceleracionPromedio(float aceleracionPromedio) {
+        this.aceleracionPromedio = aceleracionPromedio;
+    }
+
+    public float getGradoRampaSoportada() {
+        return gradoRampaSoportada;
+    }
+
+    public void setGradoRampaSoportada(float gradoRampaSoportada) {
+        this.gradoRampaSoportada = gradoRampaSoportada;
+    }
+
+    @Override
+    public void imprimirDatos() {
+        super.imprimirDatos();
+        System.out.println("Aceleración Promedio: " + aceleracionPromedio);
+        System.out.println("Grado de Rampa Soportada: " + gradoRampaSoportada);
+    }
 }
