c6251e7f08d1a4b8da7842c5be4b996b4a50ea74
amart <amart@192.168.1.147>
2025-04-06 20:19:12 -0500

    codigo base equipo

--- src/Clases/Equipo.java
+++ src/Clases/Equipo.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Clases;
 
-/**
- *
- * @author amart
- */
+package Clases;
+import java.util.ArrayList;
+
 public class Equipo {
-    
+    private String nombreEquipo;
+    private String siglas;
+    private static int tiempoTotalEquipo;
+    private ArrayList<Ciclista> ciclistas;
+
+    public Equipo(String nombreEquipo, String siglas) {
+        this.nombreEquipo = nombreEquipo;
+        this.siglas = siglas;
+        this.ciclistas = new ArrayList<>();
+    }
+
+    public String getNombreEquipo() {
+        return nombreEquipo;
+    }
+
+    public void setNombreEquipo(String nombreEquipo) {
+        this.nombreEquipo = nombreEquipo;
+    }
+
+    public String getSiglas() {
+        return siglas;
+    }
+
+    public void setSiglas(String siglas) {
+        this.siglas = siglas;
+    }
+
+    public static int getTiempoTotal() {
+        return tiempoTotalEquipo;
+    }
+
+    public static void setTiempoTotal(int tiempo) {
+        tiempoTotalEquipo = tiempo;
+    }
+
+    public void añadirCiclista(Ciclista ciclista) {
+        ciclistas.add(ciclista);
+    }
+
+    public void imprimirDatos() {
+        System.out.println("Equipo: " + nombreEquipo + " (" + siglas + ")");
+        System.out.println("Tiempo Total del Equipo: " + tiempoTotalEquipo);
+        for (Ciclista ciclista : ciclistas) {
+            ciclista.imprimirDatos();
+        }
+    }
 }
+    
+
